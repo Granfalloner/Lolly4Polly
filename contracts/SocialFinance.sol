b82072223ef588acb37743a1020e4de829b3dea6
Vasyl Myronchuk <granfalloner@gmail.com>
2023-11-26 15:04:24 +0100

    Add contribution details. Fix earnings calculations. Code reading.

--- contracts/SocialFinance.sol
+++ contracts/SocialFinance.sol
@@ -12,14 +12,20 @@ contract SocialFinance is Ownable {
         bool isRegistered;
     }
 
+    struct Contribution {
+        uint256 amount;
+        uint256 date;
+    }
+    
     struct LoanApplication {
         uint256 amount;
         uint256 interestRate;
+        uint256 createdAt;
         address borrower;
         uint256 fundedAmount;
         bool isFunded;
         bool isClosed;
-        mapping(address => uint256) contributions;
+        mapping(address => Contribution) contributions;
         address[] lenders; // Array to keep track of lenders' addresses
     }
 
@@ -27,7 +33,10 @@ contract SocialFinance is Ownable {
     mapping(address => Lender) public lenders;
     LoanApplication[] private loanApplications;
 
-    uint256 public constant PLATFORM_COMMISSION_PERCENTAGE = 1;
+    uint256 public constant PERCENTAGE_UNITS = 10_000;
+    uint256 public constant YEAR_DURATION = 60 * 60 * 24 * 365;
+    uint256 public constant PLATFORM_COMMISSION_PERCENTAGE = 1 * PERCENTAGE_UNITS / 100;
+    uint256 public constant DENOMINATOR = PERCENTAGE_UNITS * YEAR_DURATION;
 
     event BorrowerRegistered(address borrower);
     event KYCStatusUpdated(address borrower, bool isKYCCompleted);
@@ -62,12 +71,13 @@ contract SocialFinance is Ownable {
         emit LenderRegistered(msg.sender);
     }
 
-    // Create a loan application
+    // Create a loan application.
+    // _interestRate should be in PERCENTAGE_UNITS
     function createLoanApplication(uint256 _amount, uint256 _interestRate) public {
     require(borrowers[msg.sender].isKYCCompleted, "Borrower is not KYC verified");
 
     LoanApplication storage newApplication = loanApplications.push();
-    newApplication.amount = _amount;
+    newApplication.amount = _amount * 1e18;
     newApplication.interestRate = _interestRate;
     newApplication.borrower = msg.sender;
     newApplication.fundedAmount = 0;
@@ -86,17 +96,26 @@ contract SocialFinance is Ownable {
         require(!application.isClosed, "Application is closed");
         // require(application.fundedAmount.add(msg.value) <= application.amount, "Funding exceeds loan amount");
 
-        application.contributions[msg.sender] += msg.value;
+        Contribution storage contribution = application.contributions[msg.sender];
         application.fundedAmount += msg.value;
 
-        if (application.fundedAmount == application.amount) {
+        if (application.fundedAmount >= application.amount) {
             application.isFunded = true;
         }
 
-        if (application.contributions[msg.sender] == 0) {
+        if (contribution.amount == 0) {
             application.lenders.push(msg.sender);
+            contribution.amount = msg.value;
+            contribution.date = block.timestamp;
+        } else {
+            // Recalculate contribution date as if all the amount was borrowed at once
+            // contribution.amount * (block.timestamp - contribution.date) == (contribution.amount + msg.value) * (block.timestamp - newDate);
+            uint256 duration = block.timestamp - contribution.date;
+            uint256 newAmount = contribution.amount + msg.value;
+            contribution.date = block.timestamp - contribution.amount * duration / newAmount;
+            contribution.amount = newAmount;
         }
-
+        
         payable(application.borrower).transfer(msg.value);
 
         emit LoanApplicationFunded(_applicationId, msg.sender, msg.value);
@@ -107,28 +126,29 @@ contract SocialFinance is Ownable {
 
         LoanApplication storage application = loanApplications[_applicationId];
         require(msg.sender == application.borrower, "Only the borrower can close the application");
-        require(application.isFunded, "Loan application is not fully funded");
         require(!application.isClosed, "Loan application is already closed");
 
-        uint256 repaymentAmount = application.fundedAmount + (application.fundedAmount * application.interestRate) / 100;
-        uint256 platformCommission = (repaymentAmount * PLATFORM_COMMISSION_PERCENTAGE) / 100;
-        uint256 totalRepayable = repaymentAmount + platformCommission;
-
-        require(msg.value == totalRepayable, "Incorrect repayment amount");
-
-        application.isClosed = true;
+        uint256 repaymentAmount = 0;
 
         // Distribute repayments to lenders of this specific application
         for (uint256 i = 0; i < application.lenders.length; i++) {
             address lenderAddress = application.lenders[i];
-            uint256 lenderContribution = application.contributions[lenderAddress];
-            if (lenderContribution > 0) {
-                uint256 lenderRepayment = lenderContribution + (lenderContribution * application.interestRate) / 100;
+            Contribution storage lenderContribution = application.contributions[lenderAddress];
+            if (lenderContribution.amount > 0) {
+                uint256 lendDuration = block.timestamp - lenderContribution.date;
+                uint256 lenderRepayment = lenderContribution.amount + (lenderContribution.amount * lendDuration * application.interestRate) / DENOMINATOR;
+                repaymentAmount += lenderRepayment;
                 payable(lenderAddress).transfer(lenderRepayment);
                 emit RepaymentDistributed(_applicationId, lenderAddress, lenderRepayment);
             }
         }
 
+        uint256 duration = block.timestamp - application.createdAt;
+        uint256 platformCommission = (application.fundedAmount * PLATFORM_COMMISSION_PERCENTAGE * duration) / DENOMINATOR;
+        uint256 totalRepayable = repaymentAmount + platformCommission;
+        require(msg.value >= totalRepayable, "Incorrect repayment amount");
+
+        application.isClosed = true;
         emit LoanApplicationClosed(_applicationId);
     }
 
